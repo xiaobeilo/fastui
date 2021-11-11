11936e8519dbac7ebb3853f59f108aa59dc64e4e
xiaobeilo <mayday404@163.com>
2021-11-11 11:33:18 +0800

    add value

--- src/components/widget/Value.tsx
+++ src/components/widget/Value.tsx
@@ -1,1 +1,49 @@
-export const Value = () => {}
+import { Row, Col, CheckBox, Select } from 'antd'
+import { LengthUnits } from '../../defined/type'
+const { Option } = Select
+
+type Props = {
+  units: LengthUnits[]
+}
+
+export const Value = (props) => {
+	const [value, setValue] = useState<number>(0)
+	const [unit, setUnit] = useState<LengthUnits>(LengthUnits.PX)
+  const formatValue:string = value.toString() + unit;
+
+	const unitComponent = (
+		<Select>
+      {
+        [LengthUnits.PX, LengthUnits.]
+      }
+			<Option value="px">px</Option>
+			<Option value="rem">rem</Option>
+			<Option value="%">%</Option>
+			<Option value="auto">auto</Option>
+		</Select>
+	)
+
+	return (
+		<>
+			<Row>
+				<Col>
+					<CheckBox></CheckBox>
+				</Col>
+				<Col>
+					<Input
+						value={formatValue}
+						onChange={(e) => setValue(e.target.value)}
+						addonAfter={unitComponent}
+					></Input>
+				</Col>
+			</Row>
+			<Slider value={}></Slider>
+		</>
+	)
+}
+
+export const Width = () => {
+  const units:Omit<LengthUnits, LengthUnits.VH> = [LengthUnits.PX]
+
+  return <Value units={units}/>
+}
---
+++ src/defined/type.ts
@@ -0,0 +1,6 @@
+export enum LengthUnits {
+  VW = 'vw',
+  VH = 'vh',
+  PERCENT = '%',
+  PX = 'px'
+}
